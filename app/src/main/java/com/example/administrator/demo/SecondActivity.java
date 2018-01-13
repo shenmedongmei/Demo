f548a5a51d5a131aec607c1c254f654930a76c7d
jiezoukong <784882488@qq.com>
2018-01-13 16:49:25 +0800

    new class

---
+++ app/src/main/java/com/example/administrator/demo/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2018/1/13.
+ */
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("777","1");
+    }
+}
